8de627cd8473966cd593f78b19ce4cc0557f8027
vilodye1 <70298732+vilodye1@users.noreply.github.com>
2022-10-13 11:38:20 -0400

    edit

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,6 @@
 const aboutNav = document.getElementsByClassName('about-nav');
-const listItems = document.querySelector('li')
-console.log(listItems);
+const listItems = document.getElementsByClassName('about-nav-li')
+
+listItems.forEach(li => {
+    console.log(li)
+})
