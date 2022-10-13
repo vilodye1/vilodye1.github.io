9eb5b45342c6a42ab726fb1a99948196f549c04e
vilodye1 <70298732+vilodye1@users.noreply.github.com>
2022-10-13 12:30:34 -0400

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,14 @@
 const aboutNav = document.getElementsByClassName('about-nav');
 const listItems = document.querySelectorAll('.about-nav-li')
 
-
-
-
+// Tab Toggle
 listItems.forEach(li => {
     li.addEventListener('click', (e) => {
         for (i=0; i < listItems.length; i++) {
             listItems[i].className = listItems[i].className.replace("onClick", "");
         }
         e.target.classList.add('onClick');
+        e.target.classList.replace('hidden', "");
     })
+
 })
