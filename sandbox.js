c8eb609cc1201aeab481a87777ad169766fd82f3
vilodye1 <70298732+vilodye1@users.noreply.github.com>
2022-10-13 12:24:59 -0400

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -6,7 +6,9 @@ const listItems = document.querySelectorAll('.about-nav-li')
 
 listItems.forEach(li => {
     li.addEventListener('click', (e) => {
-        listItems.classList.remove('onClick');
+        for (i=0; i < listItems.length; i++) {
+            listItems[i].className = listItems[i].className.replace("onClick", "");
+        }
         e.target.classList.add('onClick');
     })
 })
