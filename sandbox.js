74747635ea175ade49e2c1476a8c9ceed4cb7398
vilodye1 <70298732+vilodye1@users.noreply.github.com>
2022-10-13 11:43:41 -0400

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,13 @@
 const aboutNav = document.getElementsByClassName('about-nav');
 const listItems = document.querySelectorAll('.about-nav-li')
 
+
+
+
 listItems.forEach(li => {
     console.log(li.innerText);
+})
+
+listItems.addEventListener('click', (li) => {
+    console.log(li)
 })
